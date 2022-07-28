ba859c847bdb49301de9c0acadd0b3aa40010a5b
Timo978 <102506582+Timo978@users.noreply.github.com>
2022-07-28 18:57:56 +0800

    7/27

--- volatility/volatility_cones.py
+++ volatility/volatility_cones.py
@@ -1,8 +1,15 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import datetime
 import plotly as py
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+from pandas_datareader import data
+
+start = datetime.datetime(2017,1,1)#获取数据的时间段-起始时间
+end = datetime.date.today()#获取数据的时间段-结束时间
+df = data.get_data_yahoo('BTC-USD', start=start, end=end)
 
 def RV_cones(df,plot,crypto):
     max = []
@@ -40,8 +47,36 @@ def RV_cones(df,plot,crypto):
     else:
         return np.array([m25,m50,m75,max])
 
-from pandas_datareader import data
-start = datetime.datetime(2017,1,1)#获取数据的时间段-起始时间
-end = datetime.date.today()#获取数据的时间段-结束时间
-df = data.get_data_yahoo('BTC-USD', start=start, end=end)
+
 RV_cones(df,plot = True,crypto=True)
+
+def rv_quantile(df,plot,crypto):
+    df['log_ret'] = np.log(df['Close'] / df['Close'].shift())
+    x = [5,10,20]
+    for i in x:
+        df[f'rv_{i}'] = df['log_ret'].rolling(i).std() * np.sqrt(365) if crypto == True else df['log_ret'].rolling(i).std() * np.sqrt(252)
+        df[f'rv_{i}_25'] = df[f'rv_{i}'].rolling(i).quantile(.25)
+        df[f'rv_{i}_50'] = df[f'rv_{i}'].rolling(i).quantile(.5)
+        df[f'rv_{i}_75'] = df[f'rv_{i}'].rolling(i).quantile(.75)
+        df[f'rv_{i}_100'] = df[f'rv_{i}'].rolling(i).quantile(1)
+        trace = go.Scatter()
+
+    if plot == True:
+        plt.figure(figsize=(28, 20))
+
+        plt.subplot(3, 1, 1)
+        plt.plot(df[['rv_5_25', 'rv_5_50', 'rv_5_75', 'rv_5_100']])
+        plt.title("RV_5 percentile")
+
+        plt.subplot(3, 1, 2)
+        plt.plot(df[['rv_10_25', 'rv_10_50', 'rv_10_75', 'rv_10_100']])
+        plt.title("RV_10 percentile")
+
+        plt.subplot(3, 1, 3)
+        plt.plot(df[['rv_20_25', 'rv_20_50', 'rv_20_75', 'rv_20_100']])
+        plt.title("RV_20 percentile")
+
+    # plt.savefig(f'./rv_quantile.png')
+    plt.show()
+
+rv_quantile(df,plot = True,crypto = True)
